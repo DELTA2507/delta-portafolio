f7d2a5ba8ccd0de98d73481f2cdb1b4dcbb32546
Joseph UM <joseph.ugalde.murillo@gmail.com>
2026-06-28 16:16:16 -0600

    ADD: Added robots.txt, Google Search Console verification file, HTML tag in Layout.astro, and added astro-sitemap-plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 import react from '@astrojs/react';
 import remarkSlugRaw from 'remark-slug';
+import sitemap from '@astrojs/sitemap';
 const remarkSlug = /** @type {any} */ (remarkSlugRaw);
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://delta-portafolio-dev.vercel.app/',
   markdown: {
     remarkPlugins: [remarkSlug],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
